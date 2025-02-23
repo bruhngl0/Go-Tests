9dc94bffe76e4b43bd7bf8a2f06b240016229b7d
bruhngl0 <adityashrm500@gmail.com>
2025-02-23 23:52:15 +0530

    go-var

---
+++ arrays/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	var a int
+	a = 5
+	fmt.Printf("%d", a)
+}
--- books/main.go
+++ books/main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"books/books" // Importing the custom package
+	"fmt"
 )
 
 func main() {
 	// Calling the Info function from books package
-	result := books.Info("for the love of go", "second edition" , 99)
+	result := books.Info("for the love of go", "second edition", 200)
 	fmt.Printf("%s\n", result)
 }
